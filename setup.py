a6d73ac9f452e955397ac0ff70cdbe225e52d396
Michaelfosse <michaelfosse9@gmail.com>
2024-05-06 08:48:13 +0200

    first commmit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo made in the context of master thesis where the aim is to predict cognitive decline using PET/MRI from ADNI',
+    author='Michael Nielsen Fosse',
+    license='MIT',
+)
